d5a449aa5c4e2f149401b38bd3e8233a0bbfbcdb
Nityom <nityomtikherr@gmail.com>
2026-05-01 12:03:17 +0530

    fix(crons): update daily generation cron time to 06:35 UTC

--- convex/crons.ts
+++ convex/crons.ts
@@ -5,7 +5,7 @@ const crons = cronJobs();
 
 crons.cron(
   "daily-generation",
-  "30 6 * * 1-5", // Monday to Friday at 06:30 UTC (12:00 PM IST)
+  "35 6 * * 1-5", // Monday to Friday at 06:35 UTC (12:05 PM IST)
   internal.generation.runAll
 );
 
