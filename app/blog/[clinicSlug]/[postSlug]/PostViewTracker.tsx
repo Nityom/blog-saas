e13f42f87768c401bcf6512d0a22e22b50ca61a6
Nityom <nityomtikherr@gmail.com>
2026-06-30 10:14:22 +0530

    feat: proxy blog via titaniumsmiles.in/blog, fix PostViewTracker absolute URL

--- app/blog/[clinicSlug]/[postSlug]/PostViewTracker.tsx
+++ app/blog/[clinicSlug]/[postSlug]/PostViewTracker.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function PostViewTracker({ clinicId, postId }: { clinicId: string, postId: string }) {
   useEffect(() => {
     // Fire and forget view tracking
-    fetch("/api/analytics/view", {
+    fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/analytics/view`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
